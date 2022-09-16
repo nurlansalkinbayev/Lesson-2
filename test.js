0fbfe7418416dc88fb3d63803ff1d91fdf3d9d5f
nurlansalkinbayev <113543225+nurlansalkinbayev@users.noreply.github.com>
2022-09-16 23:16:19 +0500

    Add files via upload
    Задачи на циклы и функции.

--- test.js
+++ test.js
@@ -1,11 +1,60 @@
-console.log('Hello world');
-for (let ex = 1; ex < 5; ex++) {
-  console.log(`----- Starting ex ${ex}. `);
-  for (let rep = 1; rep < 5; rep++) {
-    console.log(`Repetition ${rep}.`);
+// console.log('Hello world');
+// for (let ex = 1; ex < 5; ex++) {
+//   console.log(`----- Starting ex ${ex}. `);
+//   for (let rep = 1; rep < 5; rep++) {
+//     console.log(`Repetition ${rep}.`);
+//   }
+// }
+// const nested = [2, 7, [5, 6]];
+// const [y, , [k, p]] = nested;
+// console.log(y, k, p);
+// const myName = 'Salkinbayev';
+// Задачи на циклы
+for (let i = 5; i < 11; i++) {
+  console.log(i);
+};
+for (let i = 20; i >= 10; i--) {
+  if (i === 13) {
+    break;
   }
+
+  console.log(i);
+};
+
+const arrayOfNumbers = [];
+
+for (let i = 5; i < 11; i++) {
+  arrayOfNumbers[i - 5] = i;
 }
-const nested = [2, 7, [5, 6]];
-const [y, , [k, p]] = nested;
-console.log(y, k, p);
-const myName = 'Salkinbayev';
+
+numbers = [];
+for (let i = 2; i < 10; i++) {
+  if (i % 2 === 0) {
+
+    numbers[i - 2] = i;
+  }
+
+
+}
+console.log(numbers);
+
+const arr = [1, 2, 3, 4];
+const result = [];
+
+for (let i = 0; i < arr.length; i++) {
+
+  result[i] = i * 2;
+
+
+}
+console.log(result);
+// Задачи на функции
+function sayHello(name) {
+  return (`Привет ${name}`);
+}
+console.log(sayHello('Nora'));
+
+function returnNeighboringNumbers(num) {
+  return [num - 1, num, num + 1];
+}
+console.log(returnNeighboringNumbers(8));
