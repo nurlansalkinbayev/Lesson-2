af88c69bd2990150350b148418c52adb49d9f60e
nurlansalkinbayev <113543225+nurlansalkinbayev@users.noreply.github.com>
2022-09-20 22:58:55 +0500

    Add files via upload
    Добавлен скрипт по вычислению присосов

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// 'use strict';
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  year: [],
-  privat: false
-};
-const a = prompt('Один из просмотренных фильмов', '');
-b = prompt('На сколько оцените его?', '');
-c = prompt('Один из просмотренных фильмов', '');
-d = prompt('На сколько оцените его?', '');
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   year: [],
+//   privat: false
+// };
+// const a = prompt('Один из просмотренных фильмов', '');
+// const b = +prompt('На сколько оцените его?', '');
+// const c = prompt('Один из просмотренных фильмов', '');
+// const d = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+
+
+const dataBeforeSide = +prompt('Введите данные :', '');
+const dataAfterSide = +prompt('Введите данные :', '');
+const currentLoad = +prompt('Введите  нагрузку котла:', '');
+function dataOxygen(dataBeforeSide, dataAfterSide, currentLoad) {
+  let calcOxygenBefore = 21 / (21 - dataBeforeSide);
+  let calcOxygenAfter = 21 / (21 - dataAfterSide);
+  let diffBetween = calcOxygenAfter - calcOxygenBefore;
+  let dataOxygen = (diffBetween * (currentLoad / 220)) * 100;
+  console.log(`Присосы по газовоздушному тракту составляют ${Math.round(dataOxygen)} %`);
+  return dataOxygen;
+}
+
+
+
